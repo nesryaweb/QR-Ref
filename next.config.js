0dcbf0b75dbbdb4a75e5206da79327094a7ca085
Nesrya Abdulkadir <nesryaabdulkadir.dev@gmail.com>
2026-08-19 22:51:35 +0300

    fix: include chromium binaries in vercel build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
+
 const nextConfig = {
   serverExternalPackages: [
     "@sparticuz/chromium",
     "puppeteer-core",
     "puppeteer",
   ],
+
+  outputFileTracingIncludes: {
+    "/api/transcripts": [
+      "./node_modules/@sparticuz/chromium/**/*",
+    ],
+  },
 };
 
 module.exports = nextConfig;
